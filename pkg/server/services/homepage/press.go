8e841d0b6daff8524f855f4597460247b119d89a
harsh rawat <42332761+tetrex@users.noreply.github.com>
2024-09-13 15:55:13 +0530

    feat: homepage press api added

---
+++ pkg/server/services/homepage/press.go
@@ -0,0 +1,62 @@
+package homepage
+
+import (
+	"net/http"
+	"strconv"
+
+	db "github.com/LYK-INC/blog-backend-go/db/sqlc"
+	"github.com/LYK-INC/blog-backend-go/utils/types"
+	"github.com/labstack/echo/v4"
+)
+
+type PressResponse struct {
+	Msg  string  `json:"msg"`
+	Data []Press `json:"data"`
+}
+
+func responsePressFmt(d []db.GetPressesRow) []Press {
+	// Create a slice of Blog to hold the formatted response
+	formattedResponse := make([]Press, len(d))
+
+	// Loop through the db.GetPressesRow slice and map it to Blog
+	for i, b := range d {
+		formattedResponse[i] = Press{
+			PressID:                 b.PressID,
+			PublisherName:           b.PublisherName,
+			PublisherProfileImgLink: b.PublisherProfileImgLink,
+			PressThumbnailUrl:       b.PressThumbnailUrl,
+			Description:             b.Description,
+			Title:                   b.Title,
+			ExternalUrl:             b.ExternalUrl,
+			Category:                b.Category,
+			PressPublishedAt:        b.PressPublishedAt.Time,
+		}
+	}
+
+	return formattedResponse
+}
+
+// @tags			Homepage
+// @summary			Get Press articles
+// @description		returns array of articles
+// @Param   		limit query	int	false	"int valid"	minimum(1)	maximum(100)
+// @Param   		skip query	int	false	"int valid"	minimum(0)	maximum(100)
+// @accept			json
+// @produce			json
+// @success			200	{object}	PressResponse
+// @failure			500	{object}	types.ErrResponse
+// @router			/home/press [get]
+func (s *HomePageService) PressArticles(c echo.Context) error {
+	limit, _ := strconv.ParseInt(c.QueryParam("limit"), 10, 64) // base 10, bit size 64
+	skip, _ := strconv.ParseInt(c.QueryParam("skip"), 10, 64)   // base 10, bit size 64
+
+	r, err := s.Queries.GetPresses(c.Request().Context(), db.GetPressesParams{
+		Limit:  int32(limit),
+		Offset: int32(skip),
+	})
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, types.ErrResponse{Msg: "bad paramaters", Err: err.Error()})
+	}
+
+	return c.JSON(http.StatusOK, PressResponse{Msg: "blogs data", Data: responsePressFmt(r)})
+}
--- pkg/server/services/homepage/type.go
+++ pkg/server/services/homepage/type.go
@@ -13,3 +13,15 @@ type Blog struct {
 	ReadTime         int32     `json:"read_time"`
 	AuthorProfileUrl string    `json:"author_profile_url"`
 }
+
+type Press struct {
+	PressID                 int32     `json:"press_id"`
+	PublisherName           string    `json:"publisher_name"`
+	PublisherProfileImgLink string    `json:"publisher_profile_img_link"`
+	PressThumbnailUrl       string    `json:"press_thumbnail_url"`
+	Description             string    `json:"description"`
+	Title                   string    `json:"title"`
+	ExternalUrl             string    `json:"external_url"`
+	Category                []string  `json:"category"`
+	PressPublishedAt        time.Time `json:"press_published_at"`
+}
